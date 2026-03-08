2eab283302ddf4dacb2af724852c1c784e5f2be5
nyyakko <nyakonyns@gmail.com>
2026-03-08 13:28:49 -0300

    update!

--- i3blocks/scripts/battery.sh
+++ i3blocks/scripts/battery.sh
@@ -5,7 +5,7 @@ BATTERY_INFO=$(acpi -b | grep "Battery ${BATTERY}")
 BATTERY_STATE=$(echo "${BATTERY_INFO}" | grep -wo "Full\|Charging\|Discharging")
 BATTERY_POWER=$(echo "${BATTERY_INFO}" | grep -o '[0-9]\+%' | tr -d '%')
 
-if [[-n "${BATTERY}"]]; then
+if [[ -n "$BATTERY_INFO" ]]; then
     if [[ "${BATTERY_STATE}" = "Charging" ]]; then
         echo "${BATTERY_POWER}% (Charging)"
         echo "${BATTERY_POWER}% (Charging)"
